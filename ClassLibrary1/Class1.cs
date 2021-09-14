7ad50e2e642cb80f460b2862d929247504e00978
Shenol Osman <shenol.osman@utb.ecutbildning.se>
2021-09-14 14:36:31 +0200

    basic test senario

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -4,5 +4,39 @@ namespace ClassLibrary1
 {
     public class Class1
     {
+
+        /*
+         * Metod Beskrivning
+public int StringLength(params string[] array) Den här metoden ska räkna ut den sammanlagda 
+längden av alla strängar i listan.
+Tester
+• ”Hello”,”World”
+• Tom lista
+• Null
+• String[]{”Hello”,null,”World”
+         */
+        public int? StringLength(string[] strings)
+        {
+            int res = 0;
+            if (strings==null)
+            {
+                return null;
+            }
+            foreach (var str in strings)
+                res += str.Length;
+            return res;
+        }
+
+        public int GetSum(int[] numbers)
+        {
+            int res = 0;
+            foreach (var number in numbers)
+            {
+                res += number;
+            }
+
+            return res;
+        }
     }
 }
+
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,3 +1,4 @@
+using ClassLibrary1;
 using System;
 using Xunit;
 
@@ -5,45 +6,116 @@ namespace TestProject1
 {
     public class UnitTest1
     {
+        int? length;
         [Fact]
-        public void MinArrayTest()
+        public void StringTest()
         {
             //arrange
-            //sätt upp en situation
-
-
-
-            bool test = false;
-            int[] arr = new int[4];
-
+            Class1 obj = new Class1();
+           
+            string[] strings ={"Hello", "World"};
 
             //act
-            //sätt upp en situation
-            arr[0] = 3;
-
+            length = obj.StringLength(strings);
 
             //assert
-            //analysera resultatet
-            Assert.True(true, "3");
+            Assert.Equal(5+5,length);
         }
 
         [Fact]
-        public void DefaultArrayValueZeroTest()
+        public void EmpyStringTest()
         {
-            //arrange
-            //sätt upp en situation
+            Class1 obj = new Class1();
+            string[] strings = {};
 
+            length = obj.StringLength(strings);
 
-            int[] arr = new int[4];
+            Assert.Equal(0,length);
+        }
 
+        [Fact]
+        public void NullArrayTest()
+        {
+            Class1 obj = new Class1();
+            string[] strings = null;
 
-            //act
-            //donothing
+            length = obj.StringLength(strings);
+            Assert.Null(length);
+           // Assert.Equal(0,length);
+            //Assert.Throws<NullReferenceException>(() =>
+            //{
+            //    obj.StringLength(strings);
+            //});
+        }
 
+        //[Fact]
+        //public void StringNullArrayTest()
+        //{
+        //    Class1 obj = new Class1();
+        //    string[] strings = {"Hello", null, "World"};
 
-            //assert
-            //analysera resultatet
-            Assert.Equal(0, arr[0]);
+        //    length = obj.StringLength(strings);
+
+        //    Assert.Equal(0,length);
+        //}
+    }
+
+    public class UnitTest2
+    {
+         int lenght;
+        [Fact]
+        public void NullIntTest()
+        {
+            Class1 obj = new Class1();
+            int[] numbersInts = {5 , 10};
+
+            lenght = obj.GetSum(numbersInts);
+
+            Assert.Equal(15,lenght);
         }
     }
 }
+
+
+
+    //[Fact]
+    //public void MinArrayTest()
+    //{
+    //    //arrange
+    //    //sätt upp en situation
+
+
+
+    //    bool test = false;
+    //    int[] arr = new int[4];
+
+
+    //    //act
+    //    //sätt upp en situation
+    //    arr[0] = 3;
+
+
+    //    //assert
+    //    //analysera resultatet
+    //    Assert.True(true, "3");
+    //}
+
+    //[Fact]
+    //public void DefaultArrayValueZeroTest()
+    //{
+    //    //arrange
+    //    //sätt upp en situation
+
+
+    //    int[] arr = new int[4];
+
+
+    //    //act
+    //    //donothing
+
+
+    //    //assert
+    //    //analysera resultatet
+    //    Assert.Equal(0, arr[0]);
+    //}
+
