04e47946ec451f8c05b960c5606070b536a50ffc
Shenol Osman <shenol.osman@utb.ecutbildning.se>
2021-09-14 15:33:05 +0200

    bowling game test

---
+++ ClassLibrary1/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Game
+    {
+        public void Roll(int pins)
+        {
+
+        }
+
+        public int Score()
+        {
+            return -1;
+        }
+    }
+}
---
+++ TestProject1/BowlingGameTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using Xunit;
+
+namespace TestProject1
+{
+   public class BowlingGameTest
+    {
+        [Fact]
+        public void BowlingTest()
+        {
+            Game game = new();
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(0);
+            }
+            Assert.Equal(0,game.Score());
+        }
+    }
+}
